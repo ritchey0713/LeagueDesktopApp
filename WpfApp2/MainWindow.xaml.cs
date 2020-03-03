e8d8b973be1d33a6d309f262bb9b867861105cde
Dj <ritchey0713@gmail.com>
2020-03-02 19:45:01 -0500

    add data to list box

--- WpfApp2/MainWindow.xaml.cs
+++ WpfApp2/MainWindow.xaml.cs
@@ -13,6 +13,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
 
 namespace WpfApp2
 {
@@ -21,11 +23,46 @@ namespace WpfApp2
   /// </summary>
   public partial class MainWindow : Window
   {
+    SqlConnection sQLConnection;
     public MainWindow()
     {
       InitializeComponent();
 
       string connectionString = ConfigurationManager.ConnectionStrings["WpfApp2.Properties.Settings.LeagueWPFConnectionString"].ConnectionString;
+
+      sQLConnection = new SqlConnection(connectionString);
+      ShowTeams();
+    }
+
+    private void ShowTeams()
+    {
+
+      try
+      {
+        string query = "select * from Member";
+      
+        // interface like data to make tables usable by c# objs
+        SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(query, sQLConnection);
+
+        using (sqlDataAdapter)
+        {
+          DataTable memberTable = new DataTable();
+
+          sqlDataAdapter.Fill(memberTable);
+
+          // only displays one piece of data 
+          ListTeams.DisplayMemberPath = "Name";
+
+          // data to get selected item by
+          ListTeams.SelectedValuePath = "Id";
+          ListTeams.ItemsSource = memberTable.DefaultView;
+        }
+
+      } catch (Exception e)
+      {
+        MessageBox.Show(e.ToString());
+      }
+
     }
   }
 }
